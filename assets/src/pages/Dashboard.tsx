dd05350a90dc0d501d7a0b29ae59a11b2418b1af
Jorge <jsjfuentesj@gmail.com>
2022-07-08 16:50:05 -0400

    Add dummy name edit

--- assets/src/pages/Dashboard.tsx
+++ assets/src/pages/Dashboard.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Footer from "src/components/Footer";
 import Navbar from "src/components/Navbar";
 import PresenceList from "src/components/PresenceList";
 import BoardProvider from "src/contexts/BoardProvider";
+import { useAppDispatch } from "src/redux/hooks";
+import { updateSelf } from "src/redux/userStatus";
 const debug = require("debug")("app:Dashboard");
 
 export default function Dashboard() {
+  const [name, setName] = useState("");
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(updateSelf({ name }));
+  }, [name]);
+
   return (
     <BoardProvider>
       <Navbar>
@@ -23,6 +33,11 @@ export default function Dashboard() {
             <div className="bx bx-rocket animate-wiggle text-black mx-1" />{" "}
             Dashboard Page
           </div>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border border-gray-400 my-2"
+          />
           <PresenceList />
         </div>
       </div>
